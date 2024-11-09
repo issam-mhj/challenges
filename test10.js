5d5e88c836aaf20caefcf96f4dfb4860e06f65ab
issam-mhj <issammahtaj02@gmail.com>
2024-11-09 15:58:34 +0100

    test10

--- test10.js
+++ test10.js
@@ -1,28 +1,11 @@
 function shortword(words) {
-    let count=0 ;
+    Array = [""]
+    count =0;
     for(let i=0 ; i<words.length;i++){
         if (words[i].length<4){
-            words[i]="";
-        }
-    }
-    for(let i=0 ; i<words.length-1;i++){
-        if (words[i]!==""){
+            Array[count]= words[i];
             count++;
         }
     }
-    
-    let array = Array[count];
-    for(let i=0;i<array;i++){
-        for(let y=0 ; y<words.length;y++){
-            if (words[i] !== ""){
-                array[i]=words[y];
-            }
-        }
-    }
-    return array;
+    return Array;
 }
-Example:
-
-console.log(shortword(["cat", "mouse", "dog"]))
-
-shortword(["cat", "mouse", "dog"]) // Should return ["cat", "dog"]
